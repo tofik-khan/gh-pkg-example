6a07b78873fd84c46d44b5843b500241203f834e
Tofik Khan <tofik.khan@mkausa.org>
2024-01-15 20:45:45 -0800

    Update publish script to stash and switch to master

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,16 +1,17 @@
 echo "Running script publish.sh...";
 
+echo "publish.sh: Switching to master branch";
+echo " >>> Potentially stashes uncommited work";
+git stash;
+git checkout master;
+
+echo "publish.sh: Pulling latest changes";
+git pull; 
+
 echo "publish.sh: Bump version in package.json";
-## Bump version in package.json
 version=$(npm version patch);
 
 echo "publish.sh: package.json updated to version: $version";
 
-#git commit -m "$messageTitle" -m "$messageBody";
-
-
 echo "publish.sh: Pushing updated version to GitHub";
-#git push;
-
-#echo "publish.sh: Publishing to NPM registery";
-##npm publish;
+git push;
